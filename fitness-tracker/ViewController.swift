964c15e9957dd0fdf6539d11b8e3029e8c61aa2e
marajadeskywalker <maxwell.heath@uvm.edu>
2022-10-11 10:33:46 -0400

    Created files for app boilerplate from Xcode template

---
+++ fitness-tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fitness-tracker
+//
+//  Created by Max Heath on 10/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
